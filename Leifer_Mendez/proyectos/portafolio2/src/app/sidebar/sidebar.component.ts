fa1304d5a5ec696008e55a602ca2f3f0364af3b2
miguealfredo <34900833+miguealfredo@users.noreply.github.com>
2021-08-03 18:43:21 -0500

    update
    add pwa

--- Leifer_Mendez/proyectos/portafolio2/src/app/sidebar/sidebar.component.ts
+++ Leifer_Mendez/proyectos/portafolio2/src/app/sidebar/sidebar.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import Typewriter from 't-writer.js'
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent implements OnInit {
-
+export class SidebarComponent implements OnInit, AfterViewInit {
+  @ViewChild('asTitle') asTitle:ElementRef;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit(){
+    this.initEffect();
+  }
+
+  initEffect = () => {
+    const titleElement = this.asTitle.nativeElement;
+    const writer = new Typewriter(titleElement, {
+      loop: true,
+      typeColor: 'white'
+    })
+    
+    writer
+      .changeCursorColor('white')
+      .type('Miguel Salas')
+      .rest(5000)
+      .start()
   }
 
 }
